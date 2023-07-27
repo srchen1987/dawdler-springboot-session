651bdab860217335902b1127673df76cceafb171
srchen1987 <suxuan696@163.com>
2023-07-27 10:28:19 +0800

    update

--- src/main/java/com/anywide/dawdler/clientplug/web/session/conf/JedisConfig.java
+++ src/main/java/com/anywide/dawdler/clientplug/web/session/conf/JedisConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.anywide.dawdler.clientplug.web.session.conf;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -6,6 +22,14 @@ import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
+/**
+ * @author jackson.song
+ * @version V1.0
+ * @Title JedisConfig.java
+ * @Description jedis配置类
+ * @date 2023年6月23日
+ * @email suxuan696@gmail.com
+ */
 @Configuration
 @ConfigurationProperties(prefix = "session-redis")
 public class JedisConfig {
